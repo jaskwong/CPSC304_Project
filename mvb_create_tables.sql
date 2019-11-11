20996a5318f91aaab764dcda7b83c581e376284b
Jasmine Kwong <f5j1b@ugrad.cs.ubc.ca>
2019-11-11 14:33:16 -0800

    fixed create tables bugs

--- mvb_create_tables.sql
+++ mvb_create_tables.sql
@@ -1,4 +1,11 @@
-create table vehicletype (
+drop table vehicletypes cascade constraints;
+drop table vehicles cascade constraints;
+drop table reservations cascade constraints;
+drop table customers cascade constraints;
+drop table rentals cascade constraints;
+drop table rets cascade constraints;
+
+create table vehicletypes (
 	vt_name varchar(20) not null PRIMARY KEY,
 	vt_features varchar(250),
 	vt_wrate numeric(5,2),
@@ -10,72 +17,69 @@ create table vehicletype (
 	vt_krate numeric(5,2)
 );
 
-CREATE DOMAIN stat AS CHAR
-CHECK(
-   VALUE ~ 'AVAILABLE'
-OR VALUE ~ 'MAINTENANCE'
-OR VALUE ~ 'RENTED'
+create table reservations (
+    reservations_confNo integer not null PRIMARY KEY,
+    vt_name varchar(20) not null,
+    customer_dlicense integer not null,
+    reservations_fromDate date,
+    reservations_fromTime time,
+    reservations_toDate date,
+    reservations_toTime time,
+    foreign key (vt_name) references vehicletype,
+    foreign key (customer_dlicense) references customers
 );
 
-create table vehicle ( 
-	v_id: varchar(5) not null,
-	v_license: varchar(7),
-	v_make: varchar(20),
+create table rentals (
+    rentals_rid integer not null PRIMARY KEY,
+    v_id varchar(5) not null,
+    customer_dlicense integer not null,
+    rentals_fromDate date,
+    rentals_fromTime time,
+    rentals_toDate date,
+    rentals_toTime time,
+    rentals_odometer integer,
+    rentals_cardName varchar(20),
+    rentals_cardNo integer,
+    rentals_ExpDate date,
+    reservations_confNo integer,
+    foreign key (v_id) references vehicles,
+    foreign key (customer_dlicense) references customers,
+    foreign key (reservations_confNo) references reservations
+);
+
+
+create table vehicles ( 
+	v_id varchar(5) not null PRIMARY KEY,
+	v_license varchar(7),
+	v_make varchar(20),
 	v_model varchar(20),
 	v_year int,
 	v_color CHAR(20),
 	v_odometer int,
-	v_status stat,
-	FOREIGN KEY vt_name varchar(20) references vehicletype,
+	v_status int not null
+		constraint rets_fulltank check (active in (0, 1, 2)),
+	FOREIGN KEY vt_name varchar(20) references vehicletypes,
 	v_location varchar,
 	v_city varchar
 );
 
-create table ret ( 
-	FOREIGN KEY rent_rid int references rent,
-	ret_date date,
-	ret_time time,
-	ret_odometer int,
-	ret_fulltank int not null
-		constraint ret_fulltank check (active in (0, 1)),
-	ret_value numeric(5,2)
-);
-                                   
-create table reservation (
-    reservation_confNo integer not null PRIMARY KEY,
-    vehicletype_vtname varchar(20) not null,
-    customer_cellphone integer not null,
-    reservation_fromDate date,
-    reservation_fromTime time,
-    reservation_toDate date,
-    reservation_toTime time,
-    foreign key (vt_name) references vehicletype,
-    foreign key (customer_cellphone) references customer,
-);
 
-create table rent (
-    rent_rid integer not null PRIMARY KEY,
-    v_vid varchar(5) not null,
-    customer_cellphone integer not null,
-    rent_fromDate date,
-    rent_fromTime time,
-    rent_toDate date,
-    rent_toTime time,
-    rent_odometer integer,
-    rent_cardName varchar(20),
-    rent_cardNo integer not,
-    rent_ExpDate date,
-    reservation_confNo integer,
-    foreign key (v_id) references vehicle,
-    foreign key (customer_cellphone) references customer,
-    foreign key (reservation_confNo) references reservation,
-);
-
-create table customer (
-    customer_cellphone integer not null PRIMARY KEY,
+create table customers (
+    customer_cellphone integer,
     customer_name varchar(20),
     customer_address varchar(50),
-    customer_dlicense integer,
+    customer_dlicense integer not null PRIMARY KEY
+);
+
+create table rets ( 
+    rentals_rid int PRIMARY KEY,
+	rets_date date,
+	rets_time time,
+	rets_odometer int,
+	rets_fulltank int not null
+		constraint rets_fulltank check (active in (0, 1)),
+	rets_value numeric(5,2),
+	foreign key (rentals_rid) references rentals
 );
 
 commit;
