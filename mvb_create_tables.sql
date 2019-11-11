1b8ec3c283a016d6b53ba3afcc553f97052df42f
r7g1b <r7g1b@ugrad.cs.ubc.ca>
2019-11-11 13:51:02 -0800

    added tables

--- mvb_create_tables.sql
+++ mvb_create_tables.sql
@@ -1,41 +1,38 @@
-create table branch ( 
-	branch_id integer not null PRIMARY KEY,
-	branch_name varchar2(20) not null,
-	branch_addr varchar2(50),
-	branch_city varchar2(20) not null,
-	branch_phone integer 
+create table reservation (
+    reservation_confNo integer not null PRIMARY KEY,
+    vehicletype_vtname varchar(20) not null,
+    customer_cellphone integer not null,
+    reservation_fromDate date,
+    reservation_fromTime time,
+    reservation_toDate date,
+    reservation_toTime time,
+    foreign key (vt_name) references vehicletype,
+    foreign key (customer_cellphone) references customer,
 );
 
-create table driver (
-	driver_sin integer not null PRIMARY KEY,
-	driver_name varchar(20) not null,
-	driver_addr varchar(50) not null,
-	driver_city varchar(20) not null,
-	driver_birthdate date not null,
-	driver_phone integer
+create table rent (
+    rent_rid integer not null PRIMARY KEY,
+    v_vid varchar(5) not null,
+    customer_cellphone integer not null,
+    rent_fromDate date,
+    rent_fromTime time,
+    rent_toDate date,
+    rent_toTime time,
+    rent_odometer integer,
+    rent_cardName varchar(20),
+    rent_cardNo integer not,
+    rent_ExpDate date,
+    reservation_confNo integer,
+    foreign key (v_id) references vehicle,
+    foreign key (customer_cellphone) references customer,
+    foreign key (reservation_confNo) references reservation,
 );
 
-create table license ( 
-	license_no NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	driver_sin integer not null,
-	license_type char not null,
-	license_class integer,
-	license_expiry date not null,
-	issue_date date not null,
-	branch_id integer not null,
-	foreign key (driver_sin) references driver,
-	foreign key (branch_id) references branch
-);
-
-create table exam (
-	driver_sin integer not null,
-	branch_id integer not null,
-	exam_date date not null,
-	exam_type char not null,
-	exam_score integer,
-	PRIMARY KEY (driver_sin, branch_id, exam_date),
-	foreign key (driver_sin) references driver,
-	foreign key (branch_id) references branch
+create table customer (
+    customer_cellphone integer not null PRIMARY KEY,
+    customer_name varchar(20),
+    customer_address varchar(50),
+    customer_dlicense integer,
 );
 
 commit;
