24be3b5a5c047f5c99e1799bf3879f87451d23fa
Jasmine Kwong <f5j1b@ugrad.cs.ubc.ca>
2019-11-18 19:19:45 -0800

    working sql data

--- mvb_create_tables.sql
+++ mvb_create_tables.sql
@@ -45,8 +45,8 @@ create table reservations (
     customer_dlicense integer not null,
     reservations_from timestamp,
     reservations_to timestamp,
-    foreign key (vt_name) references vehicletype,
-    foreign key (customer_dlicense) references customers
+    foreign key (customer_dlicense) references customers,
+	foreign key (vt_name) references vehicletypes
 );
 
 create table rentals (
@@ -101,22 +101,22 @@ insert into customers values(7483987534, 'Tom', '6482 ert st.', 82392872);
 insert into customers values(7483747384, 'Jerry', '4356 rty st.', 84920493);
 insert into customers values(7563546223, 'Kevin', '6475 uty st.', 90865432);
 insert into customers values(3578394574, 'Leo', '7563 yrt st.', 67299832);
-insert into reservations values(67829387, 'SUV', 22291782, to_timestamp('2016-05-24 17:09:54', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2016-05-24 17:09:54', 'YYYY-MM-DD HH24:MI:SS'));
---insert into reservations values(92830192, 'SUV', 82392872, 2019-1-1 08:00:00, 2019-2-1 08:00:00);
---insert into reservations values(74829203, 'Sedan', 84920493, 2019-2-3 09:30:00, 2019-9-1 10:00:00);
---insert into reservations values(83837652, 'Sedan', 22291782, 2019-5-6 11:30:00, 2019-8-12 11:00:00);
---insert into reservations values(74739821, 'Convertible', 90865432, 2019-4-2 11:22:00, 2019-4-30 09:22:00);
---insert into reservations values(64372832, 'Convertible', 67299832, 2019-2-3 10:00:00, 2019-4-5 11:20:00);
---insert into rentals values(74839203, '098', 22291782, 2019-1-1 09:00:00, 2019-2-1 09:00:00, 50020, 'Jackson', 2839875487208973, 2020-1-1, 67829387);
---insert into rentals values(59330234, '099', 82392872, 2019-1-1 08:00:00, 2019-2-1 08:00:00, 98393, 'Tom', 6473827493875364, 2020-2-3, 92830192);
---insert into rentals values(47289183, '100', 84920493, 2019-2-3 09:30:00, 2019-9-1 10:00:00, 84593, 'Jerry', 6583748590284533,2020-3-4, 74829203);
---insert into rentals values(94839832, '101', 22291782, 2019-5-6 11:30:00, 2019-8-12 11:00:00, 94855, 'Jackson', 2839875487208973, 2020-1-1, 83837652);
---insert into rentals values(14847723, '234', 90865432, 2019-4-2 11:22:00, 2019-4-30 09:22:00, 49384, 'Kevin', 7483927463849222, 2020-3-4, 74739821);
---insert into rentals values(94728475, '573', 67299832, 2019-2-3 10:00:00, 2019-4-5 11:20:00, 34623, 'Leo', 3474839384733842, 2021-2-1, 64372832);
---insert into rets values(74839203, 2019-2-1 09:00:00, 10020, 23, 239.24);
---insert into rets values(59330234, 2019-2-1 08:00:00, 98393, 45, 345.33);
---insert into rets values(47289183, 2019-9-1 10:00:00, 84593, 34, 238.44);
---insert into rets values(94839832, 2019-8-12 11:00:00, 94855, 55, 873.44);
---insert into rets values(14847723, 2019-4-30 09:22:00, 49384, 77, 758.33);
---insert into rets values(94728475, 2019-4-5 11:20:00, 34623, 88, 839.33);
+insert into reservations values(67829387, 'SUV', 22291782, TIMESTAMP '2019-01-01 09:00:00', TIMESTAMP '2019-02-01 09:00:00');
+insert into reservations values(92830192, 'SUV', 82392872, TIMESTAMP '2019-01-01 08:00:00', TIMESTAMP '2019-02-01 08:00:00');
+insert into reservations values(74829203, 'Sedan', 84920493, TIMESTAMP '2019-02-03 09:30:00', TIMESTAMP '2019-09-01 10:00:00');
+insert into reservations values(83837652, 'Sedan', 22291782, TIMESTAMP '2019-05-06 11:30:00', TIMESTAMP '2019-08-12 11:00:00');
+insert into reservations values(74739821, 'Convertible', 90865432, TIMESTAMP '2019-04-02 11:22:00', TIMESTAMP '2019-04-30 09:22:00');
+insert into reservations values(64372832, 'Convertible', 67299832, TIMESTAMP '2019-02-03 10:00:00', TIMESTAMP '2019-04-05 11:20:00');
+insert into rentals values(74839203, '098', 22291782, TIMESTAMP '2019-01-01 09:00:00', TIMESTAMP '2019-02-01 09:00:00', 50020, 'Jackson', 2839875487208973, DATE '2020-1-1', 67829387);
+insert into rentals values(59330234, '099', 82392872, TIMESTAMP '2019-01-01 08:00:00', TIMESTAMP '2019-02-01 08:00:00', 98393, 'Tom', 6473827493875364, DATE '2020-2-3', 92830192);
+insert into rentals values(47289183, '100', 84920493, TIMESTAMP '2019-02-03 09:30:00', TIMESTAMP '2019-09-01 10:00:00', 84593, 'Jerry', 6583748590284533,DATE '2020-03-04', 74829203);
+insert into rentals values(94839832, '101', 22291782, TIMESTAMP '2019-05-06 11:30:00', TIMESTAMP '2019-08-12 11:00:00', 94855, 'Jackson', 2839875487208973, DATE '2020-01-01', 83837652);
+insert into rentals values(14847723, '234', 90865432, TIMESTAMP '2019-04-02 11:22:00', TIMESTAMP '2019-04-30 09:22:00', 49384, 'Kevin', 7483927463849222, DATE '2020-03-04', 74739821);
+insert into rentals values(94728475, '573', 67299832, TIMESTAMP '2019-02-03 10:00:00', TIMESTAMP '2019-04-05 11:20:00', 34623, 'Leo', 3474839384733842, DATE '2021-02-01', 64372832);
+insert into rets values(74839203, TIMESTAMP '2019-02-01 09:00:00', 10020, 23, 239.24);
+insert into rets values(59330234, TIMESTAMP '2019-02-01 08:00:00', 98393, 45, 345.33);
+insert into rets values(47289183, TIMESTAMP '2019-09-01 10:00:00', 84593, 34, 238.44);
+insert into rets values(94839832, TIMESTAMP '2019-08-12 11:00:00', 94855, 55, 873.44);
+insert into rets values(14847723, TIMESTAMP '2019-04-30 09:22:00', 49384, 77, 758.33);
+insert into rets values(94728475, TIMESTAMP '2019-04-05 11:20:00', 34623, 88, 839.33);
 commit;
